3658ac119f610d365b6f9fb22abae363ad707e10
Daniel Molina <danimolina@gmail.com>
2024-10-08 20:25:20 +0200

    Mejorado uso.

--- compila.sh
+++ compila.sh
@@ -1,2 +1,4 @@
-python -m http.server 8100 >/dev/null
-(ls *fast.md | entr pandoc -t revealjs *fast.md -s -o index.html --css=custom.css -V revealjs-url=https://unpkg.com/reveal.js@3.9.1/) &
+python -m http.server 8100 >/dev/null &
+#(ls *fast.md | entr pandoc -t revealjs *fast.md -s -o index.html --css=custom.css -V revealjs-url=https://unpkg.com/reveal.js@3.9.1/) &
+watchexec -e md -c -- pandoc -t revealjs *fast.md -s -o index.html
+#(ls *fast.md | entr pandoc -t revealjs *fast.md -s -o index.html) &
